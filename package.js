a62c105a1c68e54b0252315edac7ddb32701a2aa
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-06-29 17:08:50 -0700

    First commit

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  summary: "Accounts service for LinkedIn accounts"
+});
+
+Package.on_use(function(api) {
+  api.use('accounts-base', ['client', 'server']);
+
+  api.add_files(['linkedin_login_button.css'], 'client');
+
+  api.add_files('linkedin_common.js', ['client', 'server']);
+  api.add_files('linkedin_server.js', 'server');
+  api.add_files('linkedin_client.js', 'client');
+});
