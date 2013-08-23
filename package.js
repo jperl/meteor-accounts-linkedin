af40e7b4475d07082bd0289b19a01f32ac226354
Alex Duan <alexduan@gmail.com>
2013-08-23 15:42:50 -0400

    Updated package.js to include linkedin on both client and server

--- package.js
+++ package.js
@@ -5,6 +5,7 @@ Package.describe({
 Package.on_use(function(api) {
   api.use('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
+  api.use('linkedin', ['client', 'server']);
 
   api.add_files(['linkedin_login_button.css'], 'client');
 
