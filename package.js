9078837d846d3c48d9fb9ef5888e76962ddcdb90
Jon Perl <perl.jonathan@gmail.com>
2014-09-26 12:30:13 -0400

    Update dependencies
    Update to jonperl:linkedin:0.1.1 to support mondora-connect-with

--- package.js
+++ package.js
@@ -1,19 +1,23 @@
 Package.describe({
-    name: 'jonperl:accounts-linkedin',
-    summary: 'Login service for LinkedIn accounts',
-    version: '1.0.0',
-    git: 'https://github.com/jperl/meteor-accounts-linkedin'
+  name: 'jonperl:accounts-linkedin',
+  summary: 'Login service for LinkedIn accounts',
+  version: '1.1.0',
+  git: 'https://github.com/jperl/meteor-accounts-linkedin'
 });
 
-Package.on_use(function (api) {
-    api.versionsFrom('METEOR@0.9.2');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@0.9.2');
 
-    api.use('accounts-base', ['client', 'server']);
-    // Export Accounts (etc) to packages using this one.
-    api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth', ['client', 'server']);
-    api.use('jonperl:linkedin@1.0.0', ['client', 'server']);
+  var both = ['web', 'server'];
 
-    api.add_files('linkedin.js');
-    api.add_files(['linkedin_login_button.css'], 'client');
+  api.use([
+    'accounts-base', 'accounts-oauth',
+    'jonperl:linkedin@1.1.0'
+  ], both);
+  
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', both);
+
+  api.addFiles('linkedin.js', both);
+  api.addFiles('linkedin_login_button.css', 'web');
 });
